c288ee20ec252461761ddcd742677d39dd0b25dd
peirong.wu <wupeirong294@gmail.com>
2022-09-03 01:09:52 +0800

    Add async all_files

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,8 @@
-use reqwest::{multipart, Client, Body};
-use crate::response::body::{AddFileResponseBody};
-use tokio::{fs::File};
+use crate::response::body::AddFileResponseBody;
+use either::Either;
+use futures::future::{join_all, ok};
+use reqwest::{multipart, Body, Client};
+use tokio::fs::File;
 use tokio_util::codec::{BytesCodec, FramedRead};
 
 // Type Aliases
@@ -9,43 +11,66 @@ type Result<T> = std::result::Result<T, BenchmarkError>;
 // Errors
 #[derive(Debug)]
 pub enum BenchmarkError {
-  IOError(std::io::Error),
-  ReqwestError(reqwest::Error),
+    IOError(std::io::Error),
+    ReqwestError(reqwest::Error),
 }
 
 impl From<std::io::Error> for BenchmarkError {
-  fn from(error: std::io::Error) -> Self {
-      BenchmarkError::IOError(error)
-  }
+    fn from(error: std::io::Error) -> Self {
+        BenchmarkError::IOError(error)
+    }
 }
 
 impl From<reqwest::Error> for BenchmarkError {
-  fn from(error: reqwest::Error) -> Self {
-      BenchmarkError::ReqwestError(error)
-  }
+    fn from(error: reqwest::Error) -> Self {
+        BenchmarkError::ReqwestError(error)
+    }
 }
 
-
 pub struct BenchmarkClient {
-  pub http_client: Client,
+    pub http_client: Client,
 }
 
 impl BenchmarkClient {
-  pub async fn add_file(self, url_path: &str, file_path: &str) -> Result<AddFileResponseBody> {
-    let file = File::open(file_path).await?;
-    let stream = FramedRead::new(file, BytesCodec::new());
-    let body = Body::wrap_stream(stream);
-  
-    let part = multipart::Part::stream(body);
-    let form = multipart::Form::new().part("file", part);
-    let full_url = format!("{}/add", url_path);
-    let response = self.http_client.post(full_url).multipart(form).send().await?;
-    
-    Ok(response.json().await?)
-  }
+    pub async fn add_file(&self, url_path: &str, file_path: &str) -> Result<AddFileResponseBody> {
+        let file = File::open(file_path).await?;
+        let stream = FramedRead::new(file, BytesCodec::new());
+        let body = Body::wrap_stream(stream);
+
+        let part = multipart::Part::stream(body);
+        let form = multipart::Form::new().part("file", part);
+        let full_url = format!("{}/add", url_path);
+        let response = self
+            .http_client
+            .post(full_url)
+            .multipart(form)
+            .send()
+            .await?;
+
+        Ok(response.json().await?)
+    }
+
+    pub async fn add_files(
+        &self,
+        url_path: &str,
+        file_paths: Vec<&str>,
+    ) -> (Vec<AddFileResponseBody>, Vec<BenchmarkError>) {
+        let mut errors: Vec<BenchmarkError> = Vec::new();
+        let successes: Vec<_> = join_all(
+            file_paths
+                .into_iter()
+                .map(|file_path| async { self.add_file(url_path, file_path).await }),
+        )
+        .await
+        .into_iter()
+        .filter_map(|result| result.map_err(|error| errors.push(error)).ok())
+        .collect();
+
+        (successes, errors)
+    }
 }
 
 trait RemoveFile {
-  fn remove_file(self, cid: &str) -> Result<()>;
-  fn remove_files(self, cids: &[&str]) -> Result<()>;
+    fn remove_file(self, cid: &str) -> Result<()>;
+    fn remove_files(self, cids: &[&str]) -> Result<()>;
 }
--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,18 @@ const BASE_URL: &str = "http://127.0.0.1:5001/api/v0";
 
 #[tokio::main]
 async fn main() {
-  let args: Vec<String> = std::env::args().collect();
-  if args.len() != 2 {
-      println!("Usage : benchmark <file_path_to_add>");
-      std::process::exit(1);
-  }
-  let file_path = &args[1];
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("Usage : benchmark <file_path_to_add>");
+        std::process::exit(1);
+    }
+    let file_path: &str = &args[1];
 
-  let benchmark_client = client::BenchmarkClient {
-    http_client: reqwest::Client::new(),
-  };
-  let result = benchmark_client.add_file(BASE_URL, file_path).await;
+    let benchmark_client = client::BenchmarkClient {
+        http_client: reqwest::Client::new(),
+    };
+    let (successes, errors) = benchmark_client.add_files(BASE_URL, vec![file_path]).await;
 
-
-  match result {
-    Ok(response_body) => println!("{:?}", response_body),
-    Err(error) => println!("{:?}", error),
-  }
+    println!("successes: {:?}", successes);
+    println!("errors: {:?}", errors);
 }
